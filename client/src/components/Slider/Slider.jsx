b25ae0bcc2a9b8b59b1fdfb10e81d766f72ea886
iannhd <in.hadyan@gmail.com>
2023-02-17 19:29:30 +0700

    tinkering with slider

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import EastIcon from '@mui/icons-material/East';
+import WestIcon from '@mui/icons-material/West';
+import './Slider.scss'
 
 const Slider = () => {
+
+    const [currentSlide, setCurrentSlide] = useState(0)
+
      const data = [
-        '/img/pexels-cottonbro-studio-4067753',
-        '/img/pexels-cottonbro-studio-4067757',
-        '/img/pexels-cottonbro-studio-4067765'
+        '/img/pexels-cottonbro-studio-4067753.jpg',
+        '/img/pexels-cottonbro-studio-4067757.jpg',
+        '/img/pexels-cottonbro-studio-4067765.jpg'
      ]
+
+     const prevSlide = () => {
+        setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1)
+     }
+
+     const nextSlide = () => {
+        setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1)
+     }
   return (
     <div className='slider'>
-        <div className="container">
+        <div className="container" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
             <img src={data[0]} alt="" />
             <img src={data[1]} alt="" />
             <img src={data[2]} alt="" />
         </div>
         <div className="icons">
-            <div className="icon"></div>
-            <div className="icon"></div>
+            <div className="icon" onClick={prevSlide}>
+                <WestIcon />
+            </div>
+            <div className="icon" onClick={nextSlide}>
+                <EastIcon/>
+            </div>
         </div>
     </div>
   )
